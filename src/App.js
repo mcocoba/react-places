1a97e5cc9c29a214c60507c11b840f52862a2e7b
Manuel Cocoba <manuelcocoba@gmail.com>
2018-01-09 15:56:25 -0700

    adding react router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 import './App.css';
+
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
       <MuiThemeProvider>
+        <Router>
+          <div>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/login" component={Login}></Route>
+          </div>
+        </Router>
+
         <Home />
       </MuiThemeProvider>
     );
