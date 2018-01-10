915335bcf9453a23f30febd32bab34b18276288d
Manuel Cocoba <manuelcocoba@gmail.com>
2018-01-09 19:27:07 -0700

    login and navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import Home from './pages/Home';
-import Login from './pages/Login';
-
+import PlacesAppBar from './components/navigation/AppBar';
 import './App.css';
-
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
       <MuiThemeProvider>
-        <Router>
-          <div>
-            <Route exact path="/" component={Home}></Route>
-            <Route path="/login" component={Login}></Route>
-          </div>
-        </Router>
-
-        <Home />
+        <PlacesAppBar />
+        { this.props.children }
       </MuiThemeProvider>
     );
   }
---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { BrowserRouter as ReactRouter, Route } from 'react-router-dom';
+
+import App from './App';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+export default class Router extends React.Component {
+    render(){
+        return(
+            <ReactRouter>
+                <App>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/login" component={Login} />
+                </App>
+            </ReactRouter>
+        )
+    }
+}
